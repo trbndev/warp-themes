835f6c4930655ec157c9a311db5a475bbd9cee22
Torben Haack <haack.t@icloud.com>
2022-07-23 20:25:34 +0200

    Add branding

--- components/App/Title/Title.tsx
+++ components/App/Title/Title.tsx
@@ -1,10 +1,17 @@
+import { TerminalIcon } from '@heroicons/react/solid';
+
 interface Props {
 	placement: 'NAVBAR' | 'SIDEBAR';
 }
 
 function AppTitle(props: Props) {
 	const conditionalVisibility = props.placement == 'NAVBAR' ? 'lg:hidden' : 'hidden lg:block mb-6';
-	return <a className={['normal-case text-2xl font-bold', conditionalVisibility].join(' ')}>Warp Theme</a>;
+	return (
+		<a className={['normal-case text-2xl font-bold', conditionalVisibility].join(' ')}>
+			<TerminalIcon className='w-8 h-8 inline' /> Warp Theme
+			<span className='ml-2 text-xs font-normal italic'>by Torben Haack</span>
+		</a>
+	);
 }
 
 export default AppTitle;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import Drawer from '@components/App/Drawer';
 import WarpApp from '@components/Warp';
 
+import Head from 'next/head';
+
 function IndexPage() {
 	return (
 		<Drawer>
+			<Head>
+				<link
+					rel='icon'
+					href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' class='h-5 w-5' viewBox='0 0 20 20' fill='currentColor'>
+					<path fill-rule='evenodd' d='M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm3.293 1.293a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 01-1.414-1.414L7.586 10 5.293 7.707a1 1 0 010-1.414zM11 12a1 1 0 100 2h3a1 1 0 100-2h-3z' clip-rule='evenodd' />
+				</svg>"
+				/>
+				<title>Warp Theme Creator &mdash; warp-theme.vercel.app</title>
+			</Head>
 			<WarpApp />
 			<div className='landscape:hidden flex flex-row bg-orange-200 text-black/90 py-2 px-4 shadow-sm rounded mt-12 w-3/4 items-center space-x-4'>
 				<svg
