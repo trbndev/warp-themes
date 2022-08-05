c79d54161ccef2b6acf841c1fd9ef7a244000682
Torben Haack <haack.t@icloud.com>
2022-08-05 13:12:47 +0200

    Removing unsed code and changing About to GitHub

--- components/App/Navbar/Navbar.tsx
+++ components/App/Navbar/Navbar.tsx
@@ -1,60 +1,9 @@
 import AppDrawerToggleButton from '@components/App/Drawer/ToggleButton';
 import AppTitle from '../Title';
-import {
-	DownloadIcon,
-	AdjustmentsIcon,
-	CashIcon,
-	IdentificationIcon,
-	ExternalLinkIcon,
-	AtSymbolIcon,
-} from '@heroicons/react/outline';
+import { DownloadIcon, AdjustmentsIcon, CashIcon, ExternalLinkIcon, CodeIcon } from '@heroicons/react/outline';
 import { DotsVerticalIcon } from '@heroicons/react/solid';
 
-import YAML from 'json-to-pretty-yaml';
-import { useAppContext } from '@lib/AppContext';
-
 function AppNavbar() {
-	const [context, _] = useAppContext();
-
-	function downloadTheme() {
-		const theme = YAML.stringify({
-			accent: context.accent.color,
-			background: context.background.color,
-			foreground: context.foreground,
-			details: context.details,
-			terminal_colors: {
-				normal: {
-					black: context.terminal_colors.normal.black,
-					red: context.terminal_colors.normal.red,
-					green: context.terminal_colors.normal.green,
-					yellow: context.terminal_colors.normal.yellow,
-					blue: context.terminal_colors.normal.blue,
-					magenta: context.terminal_colors.normal.magenta,
-					cyan: context.terminal_colors.normal.cyan,
-					white: context.terminal_colors.normal.white,
-				},
-				bright: {
-					black: context.terminal_colors.bright.black,
-					red: context.terminal_colors.bright.red,
-					green: context.terminal_colors.bright.green,
-					yellow: context.terminal_colors.bright.yellow,
-					blue: context.terminal_colors.bright.blue,
-					magenta: context.terminal_colors.bright.magenta,
-					cyan: context.terminal_colors.bright.cyan,
-					white: context.terminal_colors.bright.white,
-				},
-			},
-		});
-		const objectURL = window.URL.createObjectURL(new Blob([theme], { type: 'application/yaml' }));
-		const downloadLink = document.createElement('a');
-		downloadLink.href = objectURL;
-		downloadLink.download = `${context.name}.yaml`;
-		document.body.appendChild(downloadLink);
-		downloadLink.click();
-		downloadLink.remove();
-		window.URL.revokeObjectURL(objectURL);
-	}
-
 	return (
 		<div className='navbar bg-base-100'>
 			<div className='flex-none'>
@@ -93,9 +42,14 @@ function AppNavbar() {
 							<span>More</span>
 						</li>
 						<li>
-							<a>
-								<IdentificationIcon className='w-6 h-6' />
-								About
+							<a
+								href='https://www.github.com/trbnhck/warp-theme'
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<CodeIcon className='w-6 h-6 inline' />
+								GitHub
+								<ExternalLinkIcon className='w-4 h-4 ml-auto' />
 							</a>
 						</li>
 						<li>
