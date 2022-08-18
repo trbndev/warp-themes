0efd454e36b02022de92e972e86a9e35c3f8ec78
Torben Haack <haack.t@icloud.com>
2022-08-18 09:36:31 +0200

    feat: :mag: Adding favicon, fixing GoatCounter Script

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 
 import AppLayout from '@components/App/Layout';
 import AppNavbar from '@components/App/Navbar';
@@ -9,14 +10,8 @@ function IndexPage() {
 	return (
 		<>
 			<Head>
-				<link
-					rel='icon'
-					href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' class='h-5 w-5' viewBox='0 0 20 20' fill='currentColor'>
-					<path fill-rule='evenodd' d='M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm3.293 1.293a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 01-1.414-1.414L7.586 10 5.293 7.707a1 1 0 010-1.414zM11 12a1 1 0 100 2h3a1 1 0 100-2h-3z' clip-rule='evenodd' />
-				</svg>"
-				/>
-
 				<title>Warp-Themes - Theme Builder for Warp</title>
+
 				<meta name='title' content='Warp-Themes - Theme Builder for Warp' />
 				<meta
 					name='description'
@@ -48,8 +43,16 @@ function IndexPage() {
 				/>
 				<meta name='keywords' content='Warp, Theme, Creator, Terminal, Color, Warp-Themes' />
 				<meta name='robots' content='index, follow' />
-			
-			    <script data-goatcounter="https://haack.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+
+				<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+				<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+				<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+				<link rel='manifest' href='/site.webmanifest' />
+				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#414141' />
+				<meta name='msapplication-TileColor' content='#603cba' />
+				<meta name='theme-color' content='#ffffff' />
+
+				<Script data-goatcounter='https://haack.goatcounter.com/count' src='//gc.zgo.at/count.js' async />
 			</Head>
 			<AppLayout Navbar={<AppNavbar />} Sidebar={<AppSidebar />} WarpApp={<WarpApp />} />
 		</>
